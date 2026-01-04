01bea7f637b45615ea7615fa382cfad20bfcc962
Haoliang Yu <hyu@esri.com>
2026-01-04 01:07:25 -0500

    fix ci

--- test/preparation/setup.ts
+++ test/preparation/setup.ts
@@ -3,7 +3,7 @@ import * as config from "config";
 import * as fs from "fs";
 import * as path from "path";
 
-const dbConfig = config.get(process.env.NODE_ENV === 'travis' ? 'travis-test' : 'test');
+const dbConfig = config.get(process.env.CI === 'true' ? 'ci-test' : 'test');
 const schema = fs.readFileSync(path.join(__dirname, 'schema.sql'), 'utf-8');
 const pool = new Pool(dbConfig);
 
